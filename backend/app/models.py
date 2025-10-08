30b6a0291e22d7857c7fe074c4498623b5abb927
Vichet Hai <vichethai8@gmail.com>
2025-10-08 22:10:46 +0700

    add team configs model

--- backend/app/models.py
+++ backend/app/models.py
@@ -45,8 +45,22 @@ class User(UserBase, table=True):
     hashed_password: str
     items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
     policies: list["Policy"] = Relationship(back_populates="owner", cascade_delete=True)
-    public_holidays: list["PublicHoliday"] = Relationship(back_populates="owner", cascade_delete=True)
-    leave_types: list["LeaveType"] = Relationship(back_populates="owner", cascade_delete=True)
+    public_holidays: list["PublicHoliday"] = Relationship(
+        back_populates="owner", cascade_delete=True
+    )
+    leave_types: list["LeaveType"] = Relationship(
+        back_populates="owner", cascade_delete=True
+    )
+    teams: list["Team"] = Relationship(
+        back_populates="owner",
+        sa_relationship_kwargs={"foreign_keys": "[Team.owner_id]"},
+        cascade_delete=True,
+    )
+    team_owners: list["Team"] = Relationship(
+        back_populates="team_owner",
+        sa_relationship_kwargs={"foreign_keys": "[Team.team_owner_id]"},
+        cascade_delete=True,
+    )
 
 
 # Properties to return via API, id is always required
@@ -116,7 +130,6 @@ class NewPassword(SQLModel):
     new_password: str = Field(min_length=8, max_length=40)
 
 
-
 # ###### Custom Models ######
 # Policy
 # Shared properties
@@ -136,6 +149,7 @@ class PolicyCreate(PolicyBase):
 # Properties to receive on item update
 class PolicyUpdate(PolicyBase):
     pass
+
 
 # Database model, database table inferred from class name
 class Policy(PolicyBase, table=True):
@@ -172,6 +186,7 @@ class PublicHolidayCreate(PublicHolidayBase):
 # Properties to receive on item update
 class PublicHolidayUpdate(PublicHolidayBase):
     pass
+
 
 # Database model, database table inferred from class name
 class PublicHoliday(PublicHolidayBase, table=True):
@@ -228,4 +243,56 @@ class LeaveTypePublic(LeaveTypeBase):
 class LeaveTypesPublic(SQLModel):
     data: list[LeaveTypePublic]
     count: int
+
+
+# Team
+# Shared properties
+class TeamBase(SQLModel):
+    title: str = Field(min_length=1, max_length=255)
+    description: str | None = Field(default=None, max_length=255)
+    is_active: bool = True
+
+
+# Properties to receive on item creation
+class TeamCreate(TeamBase):
+    pass
+
+
+# Properties to receive on item update
+class TeamUpdate(TeamBase):
+    pass
+
+
+# Database model, database table inferred from class name
+class Team(TeamBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    team_owner_id: uuid.UUID = Field(
+        foreign_key="user.id", nullable=True, ondelete="SET NULL"
+    )
+    owner_id: uuid.UUID = Field(
+        foreign_key="user.id", nullable=False, ondelete="CASCADE"
+    )
+
+    team_owner: User | None = Relationship(
+        back_populates="team_owners",
+        sa_relationship_kwargs={"foreign_keys": "Team.team_owner_id"},
+    )
+    owner: User | None = Relationship(
+        back_populates="teams",
+        sa_relationship_kwargs={"foreign_keys": "[Team.owner_id]"},
+    )
+
+
+# Properties to return via API, id is always required
+class TeamPublic(TeamBase):
+    id: uuid.UUID
+    team_owner_id: uuid.UUID
+    owner_id: uuid.UUID
+
+
+class TeamsPublic(SQLModel):
+    data: list[TeamPublic]
+    count: int
+
+
 # ###### End - Custom Models ######
