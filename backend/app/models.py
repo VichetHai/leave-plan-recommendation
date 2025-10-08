6d2bcdae0a406cf472a22dcdb8efe6a4e23d8b56
Vichet Hai <vichethai8@gmail.com>
2025-10-08 22:56:42 +0700

    change name teams model

--- backend/app/models.py
+++ backend/app/models.py
@@ -248,7 +248,7 @@ class LeaveTypesPublic(SQLModel):
 # Team
 # Shared properties
 class TeamBase(SQLModel):
-    title: str = Field(min_length=1, max_length=255)
+    name: str = Field(min_length=1, max_length=255)
     description: str | None = Field(default=None, max_length=255)
     team_owner_id: uuid.UUID = Field(
         foreign_key="user.id", nullable=False, ondelete="CASCADE"
