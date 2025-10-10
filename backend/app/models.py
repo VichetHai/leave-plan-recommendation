f158473e25828fb0ba1f86060c7daf997737b5ac
Vichet Hai <vichethai8@gmail.com>
2025-10-10 22:02:04 +0700

    move model out - Public Holiday

--- backend/app/models.py
+++ backend/app/models.py
@@ -6,6 +6,7 @@ from sqlmodel import Field, Relationship, SQLModel
 # Register Models
 from app.leave_models.leave_balance_model import LeaveBalance
 from app.leave_models.leave_type_model import LeaveType
+from app.leave_models.public_holiday_model import PublicHoliday
 from app.leave_models.team_model import Team
 
 
@@ -175,43 +176,6 @@ class PolicyPublic(PolicyBase):
 
 class PoliciesPublic(SQLModel):
     data: list[PolicyPublic]
-    count: int
-
-
-# Public Holiday
-# Shared properties
-class PublicHolidayBase(SQLModel):
-    date: str = Field(unique=True, index=True)
-    name: str = Field(default="Untitled", max_length=255)
-    description: str | None = Field(default=None, max_length=255)
-
-
-# Properties to receive on item creation
-class PublicHolidayCreate(PublicHolidayBase):
-    pass
-
-
-# Properties to receive on item update
-class PublicHolidayUpdate(PublicHolidayBase):
-    pass
-
-
-# Database model, database table inferred from class name
-class PublicHoliday(PublicHolidayBase, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="public_holidays")
-
-
-# Properties to return via API, id is always required
-class PublicHolidayPublic(PublicHolidayBase):
-    id: uuid.UUID
-
-
-class PublicHolidaysPublic(SQLModel):
-    data: list[PublicHolidayPublic]
     count: int
 
 
