3c8c068965643e272b086827f90086f0e3c2d112
Vichet Hai <vichethai8@gmail.com>
2025-10-10 22:06:11 +0700

    move model out - Policy

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,6 +5,7 @@ from sqlmodel import Field, Relationship, SQLModel
 
 # Register Models
 from app.leave_models.leave_balance_model import LeaveBalance
+from app.leave_models.leave_policy_model import Policy
 from app.leave_models.leave_type_model import LeaveType
 from app.leave_models.public_holiday_model import PublicHoliday
 from app.leave_models.team_model import Team
@@ -137,46 +138,3 @@ class TokenPayload(SQLModel):
 class NewPassword(SQLModel):
     token: str
     new_password: str = Field(min_length=8, max_length=40)
-
-
-###### Custom Models ######
-# Policy
-# Shared properties
-class PolicyBase(SQLModel):
-    code: str = Field(unique=True, index=True, max_length=255)
-    name: str = Field(default="Untitled", max_length=255)
-    value: str = Field(max_length=255)
-    description: str | None = Field(default=None, max_length=255)
-    is_active: bool = True
-
-
-# Properties to receive on item creation
-class PolicyCreate(PolicyBase):
-    pass
-
-
-# Properties to receive on item update
-class PolicyUpdate(PolicyBase):
-    pass
-
-
-# Database model, database table inferred from class name
-class Policy(PolicyBase, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="policies")
-
-
-# Properties to return via API, id is always required
-class PolicyPublic(PolicyBase):
-    id: uuid.UUID
-
-
-class PoliciesPublic(SQLModel):
-    data: list[PolicyPublic]
-    count: int
-
-
-###### End - Custom Models ######
